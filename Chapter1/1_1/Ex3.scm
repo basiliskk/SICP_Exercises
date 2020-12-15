303c554562391c020a9c331be29a4c83f94b9922
Tyler Lynn <64172375+basiliskk@users.noreply.github.com>
2020-12-14 19:38:08 -0800

    Create Ex3.scm

---
+++ Chapter1/1_1/Ex3.scm
@@ -0,0 +1,10 @@
+ (define (square x) (* x x)) 
+  
+ (define (squareSum x y) (+ (square x) (square y))) 
+  
+ (define (sumOfLargestTwoSquared x y z) 
+   (cond ((and (>= (+ x y) (+ y z)) (>= (+ x y) (+ x z))) (squareSum x y)) 
+         ((and (>= (+ x z) (+ y z)) (>= (+ x z) (+ x y))) (squareSum x z)) 
+         (else (squareSum y z)) 
+   ) 
+ )
