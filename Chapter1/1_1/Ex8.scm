b2163f5e0e259d1205115ae60154a07051d5563c
Tyler Lynn <64172375+basiliskk@users.noreply.github.com>
2020-12-18 15:45:26 -0800

    Update Ex8.scm

--- Chapter1/1_1/Ex8.scm
+++ Chapter1/1_1/Ex8.scm
@@ -1,1 +1,23 @@
-(define (square y)
+(define (square y) 
+  (y * y))
+
+(define (good-enough? guess x)
+(=(improve guess x) guess))
+
+
+(define (average x y)
+(/ (+ x y) 2))
+
+
+(define (cubert-iter guess x) 
+  (if (good-enough? guess x)
+      guess
+      (cubert-iter (improve guess x) x)))
+
+(define (improve guess x) 
+  (/ (+ (/ x (* guess guess)) (* 2 guess))
+     3))
+
+(define (cubert x) 
+  (cubert-iter 1.0 x))
+
